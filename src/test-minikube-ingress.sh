2cce54976be441200d76021bc88be19f72d9c50c
Avignan Chatterjee <again.1550@gmail.com>
2021-10-28 11:56:14 +0530

    Fix bug in test. Correct URL.

--- src/test-minikube-ingress.sh
+++ src/test-minikube-ingress.sh
@@ -1,5 +1,5 @@
 ip=`kubectl get ingress -o wide | grep -v NAME | awk {'print $4'}`
 echo "minikube ingress ip" $ip
 
-curl -X POST $ip/entity -H "Content-Type: application/json" -d '{"id": 1, "value": "test123"}'
+curl -X POST $ip/entity/ -H "Content-Type: application/json" -d '{"id": 1, "value": "test123"}'
 curl -X GET $ip/entity/1
